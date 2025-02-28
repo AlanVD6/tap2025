222ef7216e0e03d33044924b9f6f2ed2ee03957c
Alan Velazquez Diaz <ghostdiaz616@gmail.com>
2025-02-28 12:42:26 -0600

    registro

---
+++ src/main/java/vistas/Cliente.java
@@ -0,0 +1,32 @@
+package vistas;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Cliente extends Stage {
+
+    private Button btnGuardar;
+    private TextField txtNombre, txtDireccion, txtTelCte, txtEmail;
+    private VBox vBox;
+    private Scene escena;
+
+     public Cliente (){
+         this.setTitle("Registrar Cliente");
+         this.setScene(escena);
+         this.show();
+     }
+     public void CrearUI(){
+
+         txtNombre = new TextField();
+         txtDireccion = new TextField();
+         txtTelCte = new TextField();
+         txtEmail = new TextField();
+         btnGuardar = new Button("Guardar");
+
+         vBox = new VBox(txtNombre,txtDireccion,txtTelCte,txtEmail,btnGuardar);
+         escena = new Scene(vBox,120,150); //300, 200
+     }
+}
