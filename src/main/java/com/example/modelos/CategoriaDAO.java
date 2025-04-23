1b3f114f2e32ac576d738c2c6f5f97c6e43af4c4
Alessandro Hernandez Sanchez <lugoraptor1@gmail.com>
2025-04-22 23:45:51 -0600

    Creada la clase CategoriaDAO. Tuve que modificar la clase Conexion para poder darle acceso a mi base de datos. Cambiar la contraseña de 123 a 1234.

---
+++ src/main/java/com/example/modelos/CategoriaDAO.java
@@ -0,0 +1,93 @@
+package com.example.modelos;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class CategoriaDAO {
+
+    private int idCat;
+    private String categoria;
+
+    public int getIdCat(){ return idCat; }
+
+    public void setIdCat(int idCat) { this.idCat = idCat; }
+
+    public String getCategoria() { return categoria; }
+
+    public void setCategoria(String categoria) { this.categoria = categoria; }
+
+    public void INSERT (){
+
+        String query = "INSERT INTO categoria(categoria) " + "values('" + categoria +"')";
+
+        try{
+
+            Statement stmt = Conexion.connection.createStatement();
+            stmt.executeUpdate(query);
+
+        }catch(Exception e){
+
+            e.printStackTrace();
+        }
+    }
+
+    public void UPDATE(){
+
+        String query = "UPDATE categoria SET categoria = '" + categoria + "' WHERE idCat = " + idCat;
+
+        try{
+
+            Statement stmt = Conexion.connection.createStatement();
+            stmt.executeUpdate(query);
+
+        }catch(Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    public void DELETE(){
+
+        String query = "DELETE FROM categoria WHERE idCat = " + idCat;
+
+        try{
+
+            Statement stmt = Conexion.connection.createStatement();
+            stmt.executeUpdate(query);
+
+        }catch(Exception e){
+
+            e.printStackTrace();
+        }
+    }
+
+    public ObservableList<CategoriaDAO> SELECT () {
+
+        String query = "SELECT * FROM categoria";
+        ObservableList<CategoriaDAO> listaC = FXCollections.observableArrayList();
+        CategoriaDAO objC;
+
+        try{
+
+            Statement stmt = Conexion.connection.createStatement();
+            ResultSet res = stmt.executeQuery(query);
+
+            while (res.next()){
+
+                objC = new CategoriaDAO();
+                objC.setIdCat(res.getInt("idCat"));
+                objC.setCategoria(res.getString("categoria"));
+                listaC.add(objC);
+
+            }
+        }catch(Exception e){
+
+            e.printStackTrace();
+        }
+
+        return listaC;
+    }
+}
